0129961d5031ae33d01c44fefa245d4b7fe4708b
kimagure <yizhou.zhu@phd.einstein.yu.edu>
2024-05-12 14:51:53 -0700

    update

--- bamsplit.sh
+++ bamsplit.sh
@@ -8,9 +8,8 @@ out_short=`echo $bam|sed 's/.bam/_short/g'`
 out_long=`echo $bam|sed 's/.bam/_long/g'`
 
 samtools view -h $bam > ${bam}.sam
-awk -v short=$out_short -v long=$out_long 'BEGIN{OFS="\t"}/^#/{next}(NR==FNR){if($9<30||$10<30){next}if($2!=$4){next}if($5-$3<=1000&&$6=="+"&&$7=="-"){a[$1]++;next}if($5-$3>=2000){b[$1]++;next}}/^@/{print $0 > short; print $0 > long ;next}{$2+=2}($1 in a){print $0 > short}($1 in b){print $0 > long}' $pairs ${bam}.sam 
+awk -v short=$out_short -v long=$out_long 'BEGIN{OFS="\t"}/^#/{next}(NR==FNR){if($9<30||$10<30){next}if($2!=$4){next}if($5-$3<=1000&&$6=="+"&&$7=="-"){a[$1]++;next}if($5-$3>=1500){b[$1]++;next}}/^@/{print $0 > short; print $0 > long ;next}{$2+=2}($1 in a){print $0 > short}($1 in b){print $0 > long}' $pairs ${bam}.sam 
 
-#awk -v short=$out_short -v long=$out_long 'BEGIN{OFS="\t"}/^#/{next}{$2+=2}(NR==FNR){if($9<30||$10<30){next}if($2!=$4){next}if($5-$3<=1000&&$6=="+"&&$7=="-"){next}{b[$1]++;next}}/^@/{ print $0 > long ;next}($1 in b){print $0 > long}' $pairs ${bam}.sam
 samtools sort -m 6G -@ 16 -O bam $out_short > ${out_short}_1.bam
 samtools sort -m 6G -@ 16 -O bam  $out_long > ${out_long}_1.bam
 gatk --java-options "-Xmx64G" AddOrReplaceReadGroups -I ${out_short}_1.bam  -O ${out_short}.bam --RGID 4 --RGLB lib1 --RGPL Illumina --RGPU unit1 --RGSM 20
